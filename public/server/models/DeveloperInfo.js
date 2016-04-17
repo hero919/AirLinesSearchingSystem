2fd004bbfc7cb7e2b6c942421af8b63e569eda0b
ZeqingZhang <terryzhang1995@yahoo.com>
2016-04-17 15:57:51 -0400

    Fixed

--- public/server/models/DeveloperInfo.js
+++ public/server/models/DeveloperInfo.js
@@ -5,7 +5,7 @@ module.exports = {
   'facebookAuth' : {
       'clientID'        : "1179261958765026",
       'clientSecret'    : "c8016dee50452fc86ef9f76738f7a2da",
-      'callbackURL'     : "http://airlinessearchingsystem-zeqingzhang.rhcloud.com//auth/facebook/callback"
+      'callbackURL'     : "http://airlinessearchingsystem-zeqingzhang.rhcloud.com/auth/facebook/callback"
   },
     'googleAuth' : {
         'clientID'        : "1060475494658-r67rnsg9hcdvuo2r0c6nhuojs74ube2v.apps.googleusercontent.com",

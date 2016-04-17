633e0631131f16686153ed6e4f0ef4faeb59e7c9
ZeqingZhang <terryzhang1995@yahoo.com>
2016-04-17 15:55:32 -0400

    Online

--- public/server/models/DeveloperInfo.js
+++ public/server/models/DeveloperInfo.js
@@ -5,12 +5,12 @@ module.exports = {
   'facebookAuth' : {
       'clientID'        : "1179261958765026",
       'clientSecret'    : "c8016dee50452fc86ef9f76738f7a2da",
-      'callbackURL'     : "http://localhost:3000/auth/facebook/callback"
+      'callbackURL'     : "http://airlinessearchingsystem-zeqingzhang.rhcloud.com//auth/facebook/callback"
   },
     'googleAuth' : {
         'clientID'        : "1060475494658-r67rnsg9hcdvuo2r0c6nhuojs74ube2v.apps.googleusercontent.com",
         'clientSecret'    : "_Ds3xftvFRlsgUQEnIwrEUMK",
-        'callbackURL'     : "http://127.0.0.1:3000/auth/google/callback"
+        'callbackURL'     : "http://airlinessearchingsystem-zeqingzhang.rhcloud.com/auth/google/callback"
     },
     'flightStates' : {
 
